a3e636050bab09cd327a0fcdf181b45687281c2e
[FadyGamilM] <1700961@eng.asu.edu.eg>
2023-06-28 18:18:27 +0300

    FEATURE | Finish the first core domain model

---
+++ core/domain/coffee.go
@@ -0,0 +1,14 @@
+package domain
+
+import "time"
+
+type coffee struct {
+	ID         string    `json:"id"`
+	Name       string    `json:"name"`
+	Region     string    `json:"region"`
+	RoastLevel string    `json:"roast_level"`
+	Price      float64   `json:"price"`
+	GrindUnit  string    `json:"grind_unit"`
+	CreatedAt  time.Time `json:"created_at"`
+	UpdatedAt  time.Time `json:"updated_at"`
+}
